ea3eb46376c4b51f95e105decbcbd7d5867eb85a
hvhai <hunterx6323@gmail.com>
2025-03-24 14:54:34 +0700

    feat: day 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# 100 days of code python practices
+
+```
+python -m venv venv
+pip install -r requirements.txt  
+```
---
+++ day01/main.py
@@ -0,0 +1,6 @@
+print('Brand name generator')
+print('Enter the city where you were born:')
+city = input()
+print('Enter the name of your first pet:')
+pet = input()
+print('Your brand name could be ' + city + ' ' + pet)
