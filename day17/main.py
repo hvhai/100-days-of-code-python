e183c42a2311eb149fa39efe3e3405534b7d7520
hvhai <hunterx6323@gmail.com>
2025-04-27 09:08:09 +0700

    feat: day 17 quiz game

---
+++ day17/main.py
@@ -0,0 +1,45 @@
+import html
+import requests
+
+
+class QuizItem:
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+
+    def is_correct(self, answer):
+        """
+        docstring
+        """
+        return answer == self.correct_answer
+
+
+class QuizGame:
+    def __init__(self, questions):
+        self.questions = questions
+        self.score = 0
+
+    def run(self):
+        for index, item in enumerate(self.questions):
+            answer = input(item.question)
+            if item.is_correct(answer):
+                self.score += 1
+        print(f"you got {self.score} score")
+
+
+def load_question():
+    url = "https://opentdb.com/api.php?amount=10&type=boolean"
+    response = requests.get(url)
+
+    if (response.status_code == 200):
+        body = response.json()
+        return [QuizItem(html.unescape(item["question"]), item["correct_answer"]) for item in body["results"]]
+    else:
+        print("error call")
+        return []
+
+
+if __name__ == "__main__":
+
+    game = QuizGame(questions=load_question())
+    game.run()
---
+++ day17/requirement.md
@@ -0,0 +1,4 @@
+Create a quiz app in python
+get question from trivia DB via endpoint: https://opentdb.com/api.php?amount=10&type=boolean
+parse result to json
+Bind to QuizGame class and Question class
